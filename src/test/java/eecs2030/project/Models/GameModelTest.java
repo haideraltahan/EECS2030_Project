484e82a3ae07875b67b857e76e0a8a4de68a3b63
ksarbino <ksarbino@gmail.com>
2017-08-06 10:05:37 -0400

    apple test case
    Signed-off-by: ksarbino <ksarbino@gmail.com>

---
+++ src/test/java/eecs2030/project/Models/GameModelTest.java
@@ -0,0 +1,52 @@
+package eecs2030.project.Models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameModelTest {
+    @Test
+    public void initGame() throws Exception {
+    }
+
+    @Test
+    public void isInGame() throws Exception {
+    }
+
+    @Test
+    public void prepareNextMove() throws Exception {
+    }
+
+    @Test
+    public void setDirection() throws Exception {
+    }
+
+    @Test
+    public void setDifficulty() throws Exception {
+    }
+
+    @Test
+    public void upgradeDifficultyLevel() throws Exception {
+    }
+
+    @Test
+    public void ableToUpgradeDifficultyLevel() throws Exception {
+    }
+
+    @Test
+    public void getBuffers() throws Exception {
+    }
+
+    @Test
+    public void getDifficulty() throws Exception {
+    }
+
+    @Test
+    public void getSnake() throws Exception {
+    }
+
+    @Test
+    public void saveScoreToDatabase() throws Exception {
+    }
+
+}
